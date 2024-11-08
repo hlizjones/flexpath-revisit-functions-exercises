0928f1ee3df4619081b90afc80068e1e8f2acdcc
johncfrauen-lc101 <christian.frauenhoffer.cs@launchcode.org>
2024-11-08 14:16:06 -0700

    worked thru exercise 13

--- solution/exercise-solutions/exercise_07/solution.js
+++ solution/exercise-solutions/exercise_07/solution.js
@@ -3,7 +3,11 @@ function createUser(name, role = "guest") {
 }
 
 // Testing
-console.log(createUser("Alice")); // Outputs: { name: 'Alice', role: 'guest' }
-console.log(createUser("Bob", "admin")); // Outputs: { name: 'Bob', role: 'admin' }
-console.log(createUser("Charlie", undefined)); // Outputs: { name: 'Charlie', role: 'guest' }
-console.log(createUser("Dave", null)); // Outputs: { name: 'Dave', role: null }
+// Outputs: { name: 'Alice', role: 'guest' }
+console.log(createUser("Alice"));
+// Outputs: { name: 'Bob', role: 'admin' }
+console.log(createUser("Bob", "admin"));
+// Outputs: { name: 'Charlie', role: 'guest' }
+console.log(createUser("Charlie", undefined));
+// Outputs: { name: 'Dave', role: null }
+console.log(createUser("Dave", null));
